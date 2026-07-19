713c4f80bb69105add1f4659fad2f8593ae6d522
VishnuBhangale <bhangale.vishnu7@gmail.com>
2026-07-19 10:34:07 +0530

    Middleware Template using Add New Item and select dot net core
    Middleware Template using Add New Item and select dot net core and search Middleware

--- PraticeWebApplication/obj/Debug/net8.0/PraticeWebApplication.AssemblyInfo.cs
+++ PraticeWebApplication/obj/Debug/net8.0/PraticeWebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PraticeWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0912b6738fcabef1d1420c32c7232eecc3c17730")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f0d659f7e5662e82fe6e8771769ce18a87e0c74")]
 [assembly: System.Reflection.AssemblyProductAttribute("PraticeWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PraticeWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
